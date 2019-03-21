e9f1557f12444742a0af43b4f4d172f13697f234
GrayFox <87567639@qq.com>
2019-03-21 21:30:57 +0800

    added chapter01-03 of python documents

--- python/01.helloworld.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
-# helloworld
-
-print("hello world")
-print("你好，世界")
--- python/03.operator.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
-# 运算符
-
-# 数学运算符
-
-# 逻辑运算符
